5fdaac42adea7163bf23735bbdda9cc3126e441f
friberger <marie.gustafsson@gmail.com>
2018-04-23 11:45:24 +0200

    Fingerfrossa (lagt till fingrar i valbart antal och längd)

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var fixed = {x: 400, y:100}; //En fast punkt
 var huvud = {min: 30, max: 150};
 var kropp = {min: 30, max: 200};
 var lem = {min: 30, max: 100};
+var finger = {min: 5, max: 250};
+var numberOfFingers = {min:2, max:75}
 
 var hals = 0.2;
 var arm_ratio = 0.8;
@@ -20,12 +22,18 @@ function setup() {
   lSlider.position(20, 50);
   kSlider = createSlider(kropp.min, kropp.max, Math.random()*(kropp.max-kropp.min)+kropp.min);
   kSlider.position(20, 80);
+  fSlider = createSlider(finger.min, finger.max, Math.random()*(finger.max-finger.min)+finger.min);
+  fSlider.position(20, 110);
+  afSlider = createSlider(numberOfFingers.min, numberOfFingers.max, Math.random()*(numberOfFingers.max-numberOfFingers.min)+numberOfFingers.min);
+  afSlider.position(20, 170);
 }
 
 function draw() {
   var huvud = hSlider.value();
   var lemmar = lSlider.value();
   var kropp = kSlider.value();
+  var finger = fSlider.value();
+  var numberOfFingers = afSlider.value();
   background(210, 200, 240);
   ellipse(400, fixed.y-huvud/2, huvud*.8, huvud); //Huvudet
   stroke(128);
@@ -33,9 +41,33 @@ function draw() {
   line(fixed.x, fixed.y+kropp, fixed.x+lemmar, fixed.y+kropp+lemmar);
   line(fixed.x, fixed.y+kropp, fixed.x-lemmar, fixed.y+kropp+lemmar);
   line(fixed.x, fixed.y, fixed.x, fixed.y+kropp);
-  line(fixed.x, fixed.y+kropp*hals, fixed.x+lemmar*arm_ratio, fixed.y+lemmar*arm_ratio);
-  line(fixed.x, fixed.y+kropp*hals, fixed.x-lemmar*arm_ratio, fixed.y+lemmar*arm_ratio);
+  armslutVx = fixed.x+lemmar*arm_ratio
+  armslutVy = fixed.y+lemmar*arm_ratio
+  armslutHx = fixed.x-lemmar*arm_ratio
+  armslutHy = fixed.y+lemmar*arm_ratio
+  line(fixed.x, fixed.y+kropp*hals, armslutVx, armslutVy);
+  line(fixed.x, fixed.y+kropp*hals, armslutHx, armslutHy);
+  drawFingers(lemmar, finger, numberOfFingers, -1);
+  drawFingers(lemmar, finger, numberOfFingers, 1)
   text("huvud " + hSlider.value(), hSlider.x * 2 + hSlider.width, 35);
   text("lemmar " + lSlider.value(), lSlider.x * 2 + lSlider.width, 65);
   text("kropp " + kSlider.value(), kSlider.x * 2 + kSlider.width, 95);
+  text("finger " + fSlider.value(), fSlider.x * 2 + fSlider.width, 125);
+  text("antal fingrar " + afSlider.value(), afSlider.x * 2 + afSlider.width, 185);
+}
+
+function drawFingers(lemmar, finger, numberOfFingers, sida) {
+	armslut = {x: fixed.x+sida*lemmar*arm_ratio, y: fixed.y+lemmar*arm_ratio}
+	line(armslut.x, armslut.y, armslut.x + sida*finger, armslut.y); 
+	for (i = 0; i < numberOfFingers-2; i++) {
+		var degrees = 90 - (i+1)*90/(numberOfFingers-1);
+		line(armslut.x, armslut.y, armslut.x + sida*finger*sin(toRadians(degrees)), armslut.y + finger*cos(toRadians(degrees)));
+	}
+	
+	line(armslut.x, armslut.y, armslut.x, armslut.y + finger); 
+	
+}
+
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
 }
